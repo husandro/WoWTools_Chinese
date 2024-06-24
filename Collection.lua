747bacc7786ca86ca31436027be0a8627362cd8d
husandro <husandro@qq.com>
2024-06-24 19:30:53 +0200

    Update Collection.lua

--- Collection.lua
+++ Collection.lua
@@ -1,8 +1,15 @@
+local id, e= ...
 
 
 
 
-local id, d= ...
+
+
+
+
+
+
+
 
 
 
@@ -66,106 +73,98 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-local function Init()
-
-    Init_Mount()
-
-    CollectionsJournalTab1:SetText('坐骑')
-    hooksecurefunc('CollectionsJournal_UpdateSelectedTab', function(self)--设置，标题
-        local selected = CollectionsJournal_GetTab(self)
-        if selected==1 then
-            self:SetTitle('坐骑')
-        elseif selected==2 then
-            self:SetTitle('宠物手册')
-        elseif selected==3 then
-            self:SetTitle('玩具箱')
-        elseif selected==4 then
-            self:SetTitle('传家宝')
-        elseif selected==5 then
-            self:SetTitle('外观')
-        end
-    end)
-    
-
-    CollectionsJournalTab2:SetText('宠物手册')
-        PetJournalSearchBox.Instructions:SetText('搜索')
-        PetJournalFilterButtonText:SetText('过滤器')
-        local function Set_Pet_Button_Name()
-            local petID = PetJournalPetCard.petID
-            local hasPetID = petID ~= nil
-            local needsFanfare = hasPetID and C_PetJournal.PetNeedsFanfare(petID)
-            if hasPetID and petID == C_PetJournal.GetSummonedPetGUID() then
-                PetJournal.SummonButton:SetText('解散')
-            elseif needsFanfare then
-                PetJournal.SummonButton:SetText('打开')
-            else
-                PetJournal.SummonButton:SetText('召唤')
-            end
-        end
-        hooksecurefunc('PetJournal_UpdateSummonButtonState', Set_Pet_Button_Name)
-        Set_Pet_Button_Name()
-
-        local function set_PetJournalFindBattle()
-            local queueState = C_PetBattles.GetPVPMatchmakingInfo()
-            if ( queueState == "queued" or queueState == "proposal" or queueState == "suspended" ) then
-                PetJournalFindBattle:SetText('离开队列')
-            else
-                PetJournalFindBattle:SetText('搜寻战斗')
-            end
-        end
-        hooksecurefunc('PetJournalFindBattle_Update', set_PetJournalFindBattle)
-        set_PetJournalFindBattle()
-        PetJournal.PetCount.Label:SetText('宠物')
-        PetJournalSummonRandomFavoritePetButtonSpellName:SetText('召唤随机\n偏好战斗宠物')
-        PetJournalHealPetButtonSpellName:SetText('复活\n战斗宠物')
-    CollectionsJournalTab3:SetText('玩具箱')
-        ToyBox.searchBox.Instructions:SetText('搜索')
-        ToyBoxFilterButtonText:SetText('过滤器')
-        hooksecurefunc(ToyBox.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
-            self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
-        end)
-    CollectionsJournalTab4:SetText('传家宝')
-        HeirloomsJournalText:SetText('过滤器')
-        HeirloomsJournalSearchBox.Instructions:SetText('搜索')
-        hooksecurefunc(HeirloomsJournal.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
-            self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
-        end)
-    CollectionsJournalTab5:SetText('外观')
-        WardrobeCollectionFrameSearchBox.Instructions:SetText('搜索')
-        hooksecurefunc(WardrobeCollectionFrame, 'SetContainer', function(self, parent)
-            if parent == CollectionsJournal then
-                self.FilterButton:SetText('过滤器')
-            elseif parent == WardrobeFrame then
-                self.FilterButton:SetText('来源')
-            end
-        end)
-        WardrobeCollectionFrame.FilterButton.text:SetText('过滤器')
-        WardrobeCollectionFrameTab1:SetText('物品')
-            hooksecurefunc(WardrobeCollectionFrame.ItemsCollectionFrame.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
-                self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
-            end)
-        WardrobeCollectionFrameTab2:SetText('套装')
-            hooksecurefunc(WardrobeCollectionFrame.SetsTransmogFrame.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
-                self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
-            end)
-
-        WardrobeTransmogFrame.ToggleSecondaryAppearanceCheckbox.Label:SetText('两侧肩膀使用不同的幻化外观')
-
+local function Init_Pet()
     e.dia("BATTLE_PET_RENAME", {text = '重命名', button1 = '接受', button2 = '取消', button3 = '默认'})
     e.dia("BATTLE_PET_PUT_IN_CAGE", {text = '把这只宠物放入笼中？', button1 = '确定', button2 = '取消'})
     e.dia("BATTLE_PET_RELEASE", {text = "\n\n你确定要释放|cffffd200%s|r吗？\n\n", button1 = '确定', button2 = '取消'})
 
+    PetJournalSearchBox.Instructions:SetText('搜索')
+    PetJournal.FilterDropdown.Text:SetText('过滤器')
+    local function Set_Pet_Button_Name()
+        local petID = PetJournalPetCard.petID
+        local hasPetID = petID ~= nil
+        local needsFanfare = hasPetID and C_PetJournal.PetNeedsFanfare(petID)
+        if hasPetID and petID == C_PetJournal.GetSummonedPetGUID() then
+            PetJournal.SummonButton:SetText('解散')
+        elseif needsFanfare then
+            PetJournal.SummonButton:SetText('打开')
+        else
+            PetJournal.SummonButton:SetText('召唤')
+        end
+    end
+    hooksecurefunc('PetJournal_UpdateSummonButtonState', Set_Pet_Button_Name)
+    Set_Pet_Button_Name()
 
+    local function set_PetJournalFindBattle()
+        local queueState = C_PetBattles.GetPVPMatchmakingInfo()
+        if ( queueState == "queued" or queueState == "proposal" or queueState == "suspended" ) then
+            PetJournalFindBattle:SetText('离开队列')
+        else
+            PetJournalFindBattle:SetText('搜寻战斗')
+        end
+    end
+    hooksecurefunc('PetJournalFindBattle_Update', set_PetJournalFindBattle)
+    set_PetJournalFindBattle()
+    PetJournal.PetCount.Label:SetText('宠物')
+    PetJournalSummonRandomFavoritePetButtonSpellName:SetText('召唤随机\n偏好战斗宠物')
+    PetJournalHealPetButtonSpellName:SetText('复活\n战斗宠物')
+
+end
+
+
+
+
+
+
+
+
+local function Init_Toy()
     
+    --ToyBox.searchBox.Instructions:SetText('搜索')
+    e.set(ToyBox.FilterDropdown.Text)
+    hooksecurefunc(ToyBox.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
+        self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
+    end)
+end
+
+
+
+
+
+
+local function Init_Heirlooms()
+    
+    HeirloomsJournal.FilterDropdown.Text:SetText('过滤器')
+    HeirloomsJournalSearchBox.Instructions:SetText('搜索')
+    hooksecurefunc(HeirloomsJournal.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
+        self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
+    end)
+end
+
+
+
+local function Init_Wardrobe()
+    WardrobeCollectionFrameSearchBox.Instructions:SetText('搜索')
+    hooksecurefunc(WardrobeCollectionFrame, 'SetContainer', function(self, parent)
+        if parent == CollectionsJournal then
+            self.FilterButton:SetText('过滤器')
+        elseif parent == WardrobeFrame then
+            self.FilterButton:SetText('来源')
+        end
+    end)
+    WardrobeCollectionFrame.FilterButton.Text:SetText('过滤器')
+    WardrobeCollectionFrameTab1:SetText('物品')
+        hooksecurefunc(WardrobeCollectionFrame.ItemsCollectionFrame.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
+            self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
+        end)
+    WardrobeCollectionFrameTab2:SetText('套装')
+        hooksecurefunc(WardrobeCollectionFrame.SetsTransmogFrame.PagingFrame, 'Update', function(self)--Blizzard_CollectionTemplates.lua
+            self.PageText:SetFormattedText('%d/%d页', self.currentPage, self.maxPages)
+        end)
+
+    WardrobeTransmogFrame.ToggleSecondaryAppearanceCheckbox.Label:SetText('两侧肩膀使用不同的幻化外观')
+
+
 
     --试衣间
     WardrobeFrameTitleText:SetText('幻化')
@@ -187,6 +186,47 @@ local function Init()
     e.hookLable(WardrobeCollectionFrame.SetsCollectionFrame.DetailsFrame.Name)
     e.hookLable(WardrobeCollectionFrame.SetsCollectionFrame.DetailsFrame.Label)
 
+
+end
+
+
+
+
+
+local function Init()
+
+    Init_Mount()
+    Init_Pet()
+    Init_Toy()
+    Init_Heirlooms()
+    Init_Wardrobe()
+    
+    CollectionsJournalTab1:SetText('坐骑')
+    CollectionsJournalTab2:SetText('宠物手册')
+    CollectionsJournalTab3:SetText('玩具箱')
+    CollectionsJournalTab4:SetText('传家宝')
+    CollectionsJournalTab5:SetText('外观')
+
+    hooksecurefunc('CollectionsJournal_UpdateSelectedTab', function(self)--设置，标题
+        e.set(self.Text)
+        --[[local selected = CollectionsJournal_GetTab(self)
+        if selected==1 then
+            self:SetTitle('坐骑')
+        elseif selected==2 then
+            self:SetTitle('宠物手册')
+        elseif selected==3 then
+            self:SetTitle('玩具箱')
+        elseif selected==4 then
+            self:SetTitle('传家宝')
+        elseif selected==5 then
+            self:SetTitle('外观')
+        end]]
+    end)
+    
+    
+
+
+    
 
 end
 
