74124d5b8c81a139d266398ec9eec80b985188f1
husandro <husandro@qq.com>
2024-06-27 18:13:01 +0200

    1

---
+++ Mount.lua
@@ -0,0 +1,35 @@
+
+local id, e = ...
+
+
+
+
+
+
+local function Init()
+    --列表，名称
+    hooksecurefunc(MountJournal.ScrollBox, 'Updata', function(self)
+        for _, btn in pairs(self:GetFrames() or {}) do
+            e.set(btn.name)
+        end
+    end)
+end
+
+
+--###########
+--加载保存数据
+--###########
+local panel= CreateFrame("Frame")
+panel:RegisterEvent("ADDON_LOADED")
+panel:SetScript("OnEvent", function(self, _, arg1)
+    if arg1==id then
+        if C_AddOns.IsAddOnLoaded('Blizzard_Collections') then
+            self:UnregisterEvent('ADDON_LOADED')
+            Init()
+        end
+
+    elseif arg1=='Blizzard_Collections' then
+        self:UnregisterEvent('ADDON_LOADED')
+        Init()
+    end
+end)
