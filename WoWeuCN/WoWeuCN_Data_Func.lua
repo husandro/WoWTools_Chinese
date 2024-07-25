cbe32026c6024c101747145852f5bf9ef976aa1b
husandro <husandro@qq.com>
2024-07-25 09:43:46 +0200

    1

--- WoWeuCN/WoWeuCN_Data_Func.lua
+++ WoWeuCN/WoWeuCN_Data_Func.lua
@@ -30,15 +30,7 @@ e.Get_Boss_Name(journalEncounterID)
 
 
 
-if not WoWeuCN_Tooltips_EncounterData then
-    loadEncounterData()
-end
-if not WoWeuCN_Tooltips_ItemData_0 then
-    loadItemData0()
-end
-if not WoWeuCN_Tooltips_UnitData_0 then
-    loadUnitData0()
-end
+
 
 
 local function split(s, delimiter)
