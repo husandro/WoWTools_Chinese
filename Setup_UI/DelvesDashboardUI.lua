a5001cb2866f70d5c687434bba6716c9f5bb1337
husandro <husandro@qq.com>
2025-04-11 00:37:36 +0200

    1

--- Setup_UI/DelvesDashboardUI.lua
+++
@@ -1,8 +0,0 @@
-local e= select(2, ...)
---地下堡, 没加载
-EventRegistry:RegisterFrameEventAndCallback("ADDON_LOADED", function(owner, arg1)
-    if arg1=='Blizzard_DelvesDashboardUI' then
-        e.set(DelvesDashboardFrame.ButtonPanelLayoutFrame.CompanionConfigButtonPanel.CompanionConfigButton.ButtonText)
-        EventRegistry:UnregisterCallback('ADDON_LOADED', owner)
-    end
-end)
