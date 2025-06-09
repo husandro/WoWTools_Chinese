0e7b9197c77e24e5e72791c3b449b0d22b7dcba8
husandro <husandro@qq.com>
2025-06-09 12:08:12 +0200

    Update EncounterJournal_PerksProgram.lua

--- Setup_UI/EncounterJournal_PerksProgram.lua
+++ Setup_UI/EncounterJournal_PerksProgram.lua
@@ -1,28 +1,4 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    --Blizzard_PerksProgramElements.lua
+--Blizzard_PerksProgramElements.lua
 
 local function Init_Blizzard_PerksProgramElements()
 
@@ -122,26 +98,6 @@ local function Init_Blizzard_PerksProgramElements()
         end
     end)
 
-   --[[  
-    hooksecurefunc(PerksProgramSetItemDetailsScrollHeaderMixin, 'InitHeader', function(self, setInfo)
-       
-        self.perksVendorItemID = setInfo.perksVendorItemID;
-	self.numSubItems = setInfo.numSubItems;
-	self.SetName:SetText(setInfo.name);
-       
-        local name= WoWTools_ChineseMixin:SetText(setInfo.name)
-        if name then
-            self.SetName:SetText(name)
-        end
-        info= setInfo
-        for k, v in pairs(info or {}) do if v and type(v)=='table' then print('|cff00ff00---',k, '---STAR') for k2,v2 in pairs(v) do print(k2,v2) end print('|cffff0000---',k, '---END') else print(k,v) end end print('|cffff00ff——————————')1
-    end)
- ]]
---PerksProgramFrame.ProductsFrame.PerksProgramShoppingCartFrame.ItemList.ScrollBox.ScrollTarget.1b9162874d0.ItemSlotLeft
-
-
-
-
 
     --PerksProgramShoppingCartMixin 
     --Blizzard_PerksProgramElements.lua
@@ -167,10 +123,7 @@ end
 
 
 local function set_item(self)
-    if not self.itemInfo then
-        return
-    end
-    local itemName= WoWTools_ChineseMixin:GetItemName(self.itemInfo.itemID)
+    local itemName= self.itemInfo and WoWTools_ChineseMixin:GetItemName(self.itemInfo.itemID)
     if itemName then
         self.ContentsContainer.Label:SetText(itemName)
     end
@@ -186,14 +139,11 @@ end
 function WoWTools_ChineseMixin.Events:Blizzard_PerksProgram()
     Init_Blizzard_PerksProgramElements()
 
-
-
     hooksecurefunc(PerksProgramProductButtonMixin, 'SetItemInfo', set_item)
     hooksecurefunc(PerksProgramFrozenProductButtonMixin, 'SetItemInfo', set_item)
+
     self:SetLabel(PerksProgramFrame.ProductsFrame.ProductsScrollBoxContainer.NameSortButton.Label)
     self:SetLabel(PerksProgramFrame.ProductsFrame.ProductsScrollBoxContainer.PriceSortButton.Label)
-
-
 
 
     -- Blizzard_PerksProgramFooter.lua
@@ -217,9 +167,6 @@ function WoWTools_ChineseMixin.Events:Blizzard_PerksProgram()
             frame.PurchasedHistoryFrame.RefundText:SetText(refundTimeLeft);
         end
     end)
-
-
-
 
 end
 
