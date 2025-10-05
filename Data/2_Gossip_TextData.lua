30abb52e628b9864f33bad70dae9e4ca4ebb781f
husandro <husandro@qq.com>
2025-10-05 16:44:43 +0200

    Update 2_Gossip_TextData.lua

--- Data/2_Gossip_TextData.lua
+++ Data/2_Gossip_TextData.lua
@@ -427,38 +427,51 @@ WoWTools_ChineseMixin_GossipTextData_Tabs={
 
 }
 
-if not WoWTools_GossipMixin then
-    WoWTools_ChineseMixin_GossipTextData_Tabs={}
+
+
+--自定义，对话，文本
+local function Set_Gossip_Text(self, info)
+    local id= info and info.gossipOptionID
+    local data= id and WoWTools_ChineseMixin_GossipTextData_Tabs[id]
+    local text= data and data.name
+    if text then
+        if data.icon then
+            if C_Texture.GetAtlasInfo(data.icon) then
+                text= '|A:'..data.icon..':0:0|a'
+            else
+                text= '|T'..data.icon..':0|t'
+            end
+        end
+        if hex then
+            text= hex..text..'|r'
+        end
+        self:SetText(text)
+    end
 end
---[[
-[122782]={icon="Vehicle-SilvershardMines-Arrow", name="物品升级", hex=""},
-[122783]={icon="poi-transmogrifier", name="幻化师", hex=""},
-[122784]={icon="WildBattlePet-Tracker", name="兽栏管理员", hex=""},
-[122785]={icon="dragonriding-barbershop-icon-protodrake", name="幻形讲坛", hex=""},
-[122786]={icon="Profession", name="专业训练师", hex=""},
-[122787]={icon="CreationCatalyst-32x32", name="名胜地", hex=""},
-[122788]={icon="Target", name="其他大陆", hex=""},
-[122789]={icon="Mailbox", name="邮箱", hex=""},
-[122790]={icon="Innkeeper", name="旅店", hex=""},
-[122791]={icon="TaxiNode_Alliance", name="飞行管理员", hex=""},
-[122792]={icon="Professions-Crafting-Orders-Icon", name="制造订单", hex=""},
-[122793]={icon="WildBattlePetCapturable", name="战斗宠物训练师", hex=""},
-[122794]={icon="poi-transmogrifier", name="理发师", hex=""},
-[122795]={icon="Banker", name="银行", hex=""},
-[122796]={icon="Auctioneer", name="拍卖行", hex=""},
-[122801]={icon="4620681", name="裁缝", hex=""},
-[122802]={icon="4620680", name="剥皮", hex=""},
-[122803]={icon="4620679", name="采矿", hex=""},
-[122804]={icon="4620678", name="制皮", hex=""},
-[122805]={icon="4620677", name="珠宝加工", hex=""},
-[122806]={icon="4620676", name="铭文", hex=""},
-[122807]={icon="4620675", name="草药学", hex=""},
-[122808]={icon="4620674", name="钓鱼", hex=""},
-[122809]={icon="4620673", name="工程学", hex=""},
-[122810]={icon="4620672", name="附魔", hex=""},
-[122811]={icon="4620671", name="烹饪", hex=""},
-[122812]={icon="4620670", name="锻造", hex=""},
-[122813]={icon="4620669", name="炼金术", hex=""},
-[123984]={icon="trading-post-minimap-icon", name="商栈", hex=""},
-[124426]={icon="gficon-chest-evergreen-greatvault-collect", name="宏伟宝库", hex=""},
-]]
+local function Init()
+
+
+    hooksecurefunc(GossipOptionButtonMixin, 'Setup', function(...)--GossipFrameShared.lua
+        Set_Gossip_Text(...)
+    end)
+    hooksecurefunc(GossipSharedAvailableQuestButtonMixin, 'Setup', function(...)
+        Set_Gossip_Text(...)
+    end)
+    WoWTools_DataMixin:Hook(GossipSharedActiveQuestButtonMixin, 'Setup', function(...)
+        Set_Gossip_Text(...)
+    end)
+
+
+
+    Init=function()end
+end
+
+
+EventRegistry:RegisterFrameEventAndCallback("PLAYER_ENTERING_WORLD", function(owner)
+    if not WoWTools_GossipMixin:Get_GossipData() then
+        Init()
+    else
+        Init=function()end
+    end
+    EventRegistry:UnregisterCallback('PLAYER_ENTERING_WORLD', owner)
+end)
