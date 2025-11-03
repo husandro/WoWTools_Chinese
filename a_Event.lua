570da5b2964199d3793c43ab1bca397458552ccd
husandro <husandro@qq.com>
2025-11-03 10:45:16 +0100

    Update a_Event.lua

--- a_Event.lua
+++ a_Event.lua
@@ -1187,6 +1187,8 @@ end
 
 function WoWTools_MoveMixin.Events:Blizzard_HousingCornerstone()
     self:SetFrame(HousingCornerstoneVisitorFrame)
+
+    self:SetFrame(HousingCornerstonePurchaseFrame)
 end
 
 --住宅搜索器
@@ -1197,4 +1199,46 @@ function WoWTools_ChineseMixin.Events:Blizzard_HousingHouseFinder()
     self:SetFrame(HouseFinderFrame.PlotInfoFrame)
     self:SetLabel(HouseFinderFrame.PlotInfoFrame.BackButton.ButtonLabel)
     self:SetButton(HouseFinderFrame.PlotInfoFrame.VisitHouseButton)
+end
+
+function WoWTools_ChineseMixin.Events:Blizzard_HousingHouseSettings()
+    self:SetLabel(HousingHouseSettingsFrame.Title)
+    self:SetButton(HousingHouseSettingsFrame.AbandonHouseButton)
+
+    self:SetLabel(HousingHouseSettingsFrame.HouseOwnerLabel)
+    self:SetLabel(HousingHouseSettingsFrame.PlotAccess.Label)
+    self:SetLabel(HousingHouseSettingsFrame.HouseAccess.Label)
+    for _, option in pairs(HousingHouseSettingsFrame.PlotAccess.accessOptions or {}) do
+        self:SetLabel(option.OptionLabel)
+    end
+        for _, option in pairs(HousingHouseSettingsFrame.HouseAccess.accessOptions or {}) do
+        self:SetLabel(option.OptionLabel)
+    end
+
+    self:SetButton(HousingHouseSettingsFrame.IgnoreListButton)
+    self:SetButton(HousingHouseSettingsFrame.SaveButton)
+end
+
+function WoWTools_ChineseMixin.Events:Blizzard_HouseEditor()
+    self:SetLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.PlaceInstruction.InstructionText)
+    
+    self:SetLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.RotateLeftInstruction.InstructionText)
+    self:HookLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.RotateLeftInstruction.Control.Text)
+
+    self:SetLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.RotateRightInstruction.InstructionText)
+    self:HookLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.RotateRightInstruction.Control.Text)
+
+    self:SetLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.RemoveInstruction.InstructionText)
+    self:HookLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.RemoveInstruction.Control.Text)
+
+    self:SetLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.ToggleGridInstruction.InstructionText)
+    self:HookLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.ToggleGridInstruction.Control.Text)
+
+    self:SetLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.CancelInstruction.InstructionText)
+    self:HookLabel(HouseEditorFrame.BasicDecorModeFrame.Instructions.CancelInstruction.Control.Text)
+
+    self:SetTabButton(HouseEditorFrame.StoragePanel)
+
+    self:SetLabel(HouseEditorFrame.StoragePanel.OptionsContainer.CategoryText)
+    self:SetLabel(HouseEditorFrame.StoragePanel.SearchBox.Instructions)
 end
